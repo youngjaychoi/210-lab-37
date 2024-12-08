8b4d805ef2572250d2dcf8d56e74eca574dad5fd
youngjaychoi <youngjaychoi02@gmail.com>
2024-12-07 22:01:12 -0800

    commit1

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    char a = 'A';
+    cout << a << endl;
+    cout << (int)a << endl;
+
+    int b = 66;
+    cout << b << endl;
+    cout << (char)b << endl;
+
+
+
+
+
+
+
+    
+    return 0;
+}
+/*
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
